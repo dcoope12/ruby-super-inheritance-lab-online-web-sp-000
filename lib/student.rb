718782b6af785c4c443e9ac79a3676192a8230f3
Devin Cooper <dcoope12@asu.edu>
2020-10-06 02:21:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,4 @@
-* Write a method in the `Student` class, `#hello`, that `puts` out the phrase: `"Hey there! I'm so excited to learn stuff."`
-* Write a method in the `Student` class, `raise_hand`, that `puts` out the phrase: `"Pick me!"`
+
 class Student
  def hello
 puts "Hey there! I'm so excited to learn stuff."
